b34249c2e3d4385060da4620bafd4ed165fad7a6
joaoUltPc <zn_john@hotmail.com>
2017-08-09 15:20:14 -0300

    Fixed some bugs

--- HitBtc.py
+++ HitBtc.py
@@ -41,6 +41,15 @@ class public_api(object):
         response = requests.get( self.url +"/api/1/public/"+tpair+"/trades")
         print(response.content)
         return json.loads(response.content)
+
+    #dosent work yet
+    def tradesTimeStamp(self,tpair,beginPeriod,endPeriod):
+        request = self.url + "/api/1/public/" + tpair+"/trades?"
+        request = request + "from="+beginPeriod+"&till="+endPeriod     #"from=0&by=trade_id&sort=desc&start_index=0&max_results=100"
+        request = request + "&by=trade_id" + "&format_timestamp=second"
+        response = requests.get( request)
+        print(response.content)
+        return json.loads(response.content)
         
     
         
@@ -50,8 +59,4 @@ class trade_api:
 
 
 
-hit= public_api()
-respost = hit.time()
 
-print(respost)
-#print(respost['timestamp'])
--- example.py
+++ example.py
@@ -4,3 +4,11 @@ from HitBtc import public_api, trade_api
 api_key    = 'secret'
 api_secret = 'secret'
 nonce      = 1
+
+
+
+hit= public_api()
+respost = hit.time()
+
+print(respost)
+#print(respost['timestamp'])
