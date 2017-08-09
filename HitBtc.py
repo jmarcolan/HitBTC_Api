cf869bb59a70c1a1ba970b9b11775827c8dcf36d
João Antônio Marcolan <jamarcolan@gmail.com>
2017-08-09 03:22:44 -0300

    Start class
    Start class to use hitBTC api with python 3

---
+++ HitBtc.py
@@ -0,0 +1,56 @@
+
+#HitBtc API Class
+#Developed by jmarcolan in Python 3
+#https://github.com/acidvegas/btc-e
+
+'''
+API Documentation:
+ * Public API v1 (https://hitbtc.com/api#marketrestful)
+'''
+
+import json, requests
+
+
+class public_api(object):
+    def __init__(self):
+        self.url= 'http://api.hitbtc.com'
+
+    def time(self):
+        response = requests.get( self.url + "/api/1/public/time")
+        print(response.content)
+        return json.loads(response.content)
+    
+    def symbols(self):
+        response = requests.get( self.url +"/api/1/public/symbols")
+        print(response.content)
+        return json.loads(response.content)
+
+    def ticker(self,tpair):
+        # example hit.ticker("ETHUSD")
+        response = requests.get( self.url +"/api/1/public/"+tpair+"/ticker")
+        print(response.content)
+        return json.loads(response.content)
+    
+    def orderbook(self, tpair):
+        response = requests.get( self.url +"/api/1/public/"+tpair+"/orderbook")
+        print(response.content)
+        return json.loads(response.content)
+    
+    def trades(self, tpair):
+        response = requests.get( self.url +"/api/1/public/"+tpair+"/trades")
+        print(response.content)
+        return json.loads(response.content)
+        
+    
+        
+class trade_api:
+    def __init__(self, name):
+        self.key = name
+
+
+
+hit= public_api()
+respost = hit.time()
+
+print(respost)
+#print(respost['timestamp'])
